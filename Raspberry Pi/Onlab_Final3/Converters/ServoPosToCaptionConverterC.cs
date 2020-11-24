ab3f4a5dbaa4dd641da0e77bf08827a1344a029b
battistonir <66639764+battistonir@users.noreply.github.com>
2020-11-24 15:23:30 -0500

    Add files via upload
    Additional files uploaded

---
+++ Raspberry Pi/Onlab_Final3/Converters/ServoPosToCaptionConverterC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Onlab_Final3.Converters
+{
+    class ServoPosToCaptionConverterC : IValueConverter
+    {
+        /// <summary>
+        /// Converter that converts the caption of the button
+        /// that changes the raspberry driven relay state in order to match the action
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(bool)value)
+            {
+                return "Water C";
+            }
+            else
+            {
+                return "Water D";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
